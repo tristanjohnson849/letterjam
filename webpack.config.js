a73dfaa3217e7cb02768a28ffaac468ab71573ae
Tristan Johnson <tristan.johnson849@gmail.com>
2023-01-29 20:43:49 -0800

    Fix css

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 loader: 'babel-loader'
+            },{
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
             },
         ],
     },
