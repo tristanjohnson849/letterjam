78f252858e5ce03b6d72efed555da1db04a2d917
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-04 02:10:43 -0800

    Add missing files from last commit

---
+++ src/Game/PlayerCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlayerCard = () => {
+
+    return (
+        <h1 style={{ width: '100%', textAlign: 'center' }}>Player Card</h1>
+    );
+};
+
+export default PlayerCard;
