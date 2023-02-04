f46041fdddbb8613c205b82e975b332257116836
Tristan Johnson <tristan.johnson849@gmail.com>
2023-02-03 19:34:55 -0800

    More folder style tweaks

--- src/Game/PlayerCardFolder.tsx
+++ src/Game/PlayerCardFolder.tsx
@@ -3,25 +3,25 @@ import React, { useRef, useState } from "react";
 
 const PlayerCardFolder: React.FC<{}> = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [opening, setIsOpening] = useState(false);
+    const [animating, setIsAnimating] = useState<'opening'|'closing'|null>(null);
 
-    const onClick = () => {
+    const onClick = (animating: 'opening'|'closing'|null)=> () => {
         setIsOpen(prev => !prev);
-        setIsOpening(true);
+        setIsAnimating(animating);
     };
 
     return (
         <div>
             <div
                 className={`player-card-folder player-card-folder-${isOpen ? 'open' : ''}`}
-                onClick={onClick}
+                onClick={!isOpen && onClick('opening')}
                 style={{ zIndex: 10 }}
             >
                 <div className="folder-back"/>
             </div>
             <div
                 className={`player-card-folder player-card-folder-${isOpen ? 'open' : ''}`}
-                onClick={onClick}
+                onClick={!isOpen && onClick('opening')}
                 style={{ zIndex: 13 }}
             >
                 <div
@@ -29,25 +29,35 @@ const PlayerCardFolder: React.FC<{}> = () => {
                     style={{ position: 'relative' }}
                 >
                     <div
-                        className={`folder-front ${opening ? 'folder-front-opening' : ''}`}
-                        onTransitionEnd={() => setIsOpening(false)}
+                        className={`folder-front ${animating ? 'folder-front-opening' : ''}`}
                     >
                         <div className="folder-front-panel"/>
                         <div className="folder-front-tab"/>
                     </div>
                 </div>
             </div>
-            <div className="flip-container full-size" style={{ position: 'fixed', top: 0, left: 0, zIndex: isOpen ? 14 : 12 }}>
+            <div className="flip-container full-size" style={{ 
+                position: 'fixed', 
+                top: 0, 
+                left: 0, 
+                zIndex: isOpen || animating === 'closing' ? 14 : 12 }}>
                 <div
-                    className={`player-card-container ${opening ?
-                        'player-card-container-opening'
+                    onTransitionEnd={() => setIsAnimating(null)}
+                    className={`player-card-container ${animating 
+                        ? 'player-card-container-opening'
                         : isOpen
                             ? 'player-card-container-open'
                             : ''
                         }`}
                 >
                     <h1 style={{ width: '100%', textAlign: 'center' }}>Player Card</h1>
-                    <button style={{ position: 'absolute', top: '12px', right: '12px'}} onClick={onClick} disabled={!isOpen}>X</button>
+                    <button 
+                        style={{ position: 'absolute', top: '12px', right: '12px'}} 
+                        onClick={onClick('closing')} 
+                        disabled={!isOpen}
+                    >
+                        X
+                    </button>
                 </div>
             </div>
         </div>
